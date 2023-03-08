9e6e3ff40d3bb7a9f248e10fa8eb0071698f1be1
wangkc <wkclz@qq.com>
2023-03-08 22:52:55 +0800

    sku 展示，和批量设置

--- src/views/product/product/components/editSku.vue
+++ src/views/product/product/components/editSku.vue
@@ -1,31 +1,9 @@
 <template>
-  <el-dialog :title="title" v-model="open" width="1200px" append-to-body draggable :close-on-click-modal="false">
+  <el-dialog :title="title" v-model="open" width="1280px" append-to-body draggable :close-on-click-modal="false">
 
-    <!--
-    <el-form :inline="true" :model="batchSkuData" class="demo-form-inline" >
-      <el-form-item label="总价格">
-        <el-input-number v-model="batchSkuData.originPrice" controls-position="right" placeholder="总价格"  size="mini"  />
-      </el-form-item>
-      <el-form-item label="促销价格">
-        <el-input-number v-model="batchSkuData.promotionPrice" controls-position="right" placeholder="促销价格"  size="mini" />
-      </el-form-item>
-      <el-form-item label="订金">
-        <el-input-number v-model="batchSkuData.deposit" controls-position="right" placeholder="订金" size="mini"/>
-      </el-form-item>
-      <el-form-item label="库存">
-        <el-input-number v-model="batchSkuData.stock" controls-position="right" placeholder="库存" size="mini"/>
-      </el-form-item>
-      <el-form-item label="预设销量">
-        <el-input-number v-model="batchSkuData.salePre" controls-position="right" placeholder="预设销量" size="mini"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="batchSetSock" size="small">批量设置</el-button>
-      </el-form-item>
-    </el-form>
-    -->
 
     <el-row :gutter="20">
-      <el-col :span="6">
+      <el-col :span="5">
         <el-tree
             ref="propTreeRef"
             default-expand-all
@@ -36,8 +14,8 @@
 
             <!-- 新增 -->
             <span v-if="isCreate">
-              <input v-if="node.level === 1" v-model="data.propName" @input="inputNode(node, data)" style="width: 160px; "/>
-              <input v-if="node.level === 2" v-model="data.itemValue" @input="inputNode(node, data)" style="width: 160px; "/>
+              <el-input size="small" v-if="node.level === 1" v-model="data.propName" @input="inputNode(node, data)" style="width: 160px; "/>
+              <el-input size="small" v-if="node.level === 2" v-model="data.itemValue" @input="inputNode(node, data)" style="width: 160px; "/>
 
               <!-- 超过1项的，展示 - 图标 -->
               <el-icon v-if="node.level === 1 && form.props.length > 1" @click="removeNode(node, data)"><remove-filled /></el-icon>
@@ -79,10 +57,32 @@
           </p>
         -->
       </el-col>
-      <el-col :span="18">
+      <el-col :span="19">
+
+        <el-form :inline="true" :model="batchSkuData" class="demo-form-inline" >
+          <el-form-item label="总价">
+            <el-input-number size="small" v-model="batchSkuData.originPrice" controls-position="right" placeholder="总价格" style="width: 100px;"/>
+          </el-form-item>
+          <el-form-item label="促销价">
+            <el-input-number size="small" v-model="batchSkuData.promotionPrice" controls-position="right" placeholder="促销价格" style="width: 100px;"/>
+          </el-form-item>
+          <el-form-item label="订金">
+            <el-input-number size="small" v-model="batchSkuData.deposit" controls-position="right" placeholder="订金"  style="width: 100px;"/>
+          </el-form-item>
+          <el-form-item label="库存">
+            <el-input-number size="small" v-model="batchSkuData.stock" controls-position="right" placeholder="库存"  style="width: 80px;"/>
+          </el-form-item>
+          <el-form-item label="预销量">
+            <el-input-number size="small" v-model="batchSkuData.salePre" controls-position="right" placeholder="预销量"  style="width: 90px;"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="batchSetSock" size="small">批量设置</el-button>
+          </el-form-item>
+        </el-form>
+
         <div class="grid-content bg-purple">
           <el-table :data="form.skuStocks" border size="small">
-            <el-table-column label="规格" prop="stockDesc" :show-overflow-tooltip="true" min-width="200"/>
+            <el-table-column label="规格" prop="stockDesc" :show-overflow-tooltip="true" min-width="160"/>
             <!--
             <el-table-column label="图片" prop="pic" :show-overflow-tooltip="false" min-width="60">
               <template #default="scope">
@@ -100,27 +100,27 @@
             -->
             <el-table-column label="总价格" prop="originPrice" :show-overflow-tooltip="true" min-width="100">
               <template #default="scope">
-                <el-input size="small" type="number" v-model="scope.row.originPrice" placeholder="总价格"/>
+                <el-input-number size="small" type="number" v-model="scope.row.originPrice" controls-position="right" placeholder="总价格"/>
               </template>
             </el-table-column>
             <el-table-column label="促销价格" prop="promotionPrice" :show-overflow-tooltip="true" min-width="100">
               <template #default="scope">
-                <el-input size="small" type="number" v-model="scope.row.promotionPrice" placeholder="促销价格"/>
+                <el-input-number size="small" type="number" v-model="scope.row.promotionPrice" controls-position="right" placeholder="促销价格"/>
               </template>
             </el-table-column>
             <el-table-column label="订金" prop="deposit" :show-overflow-tooltip="true" min-width="100">
               <template #default="scope">
-                <el-input size="small" type="number" v-model="scope.row.deposit" placeholder="订金"/>
+                <el-input-number size="small" type="number" v-model="scope.row.deposit" controls-position="right" placeholder="订金"/>
               </template>
             </el-table-column>
             <el-table-column label="库存" prop="stock" :show-overflow-tooltip="true" min-width="100">
               <template #default="scope">
-                <el-input size="small" type="number" v-model="scope.row.stock" placeholder="库存"/>
+                <el-input-number size="small" type="number" v-model="scope.row.stock" controls-position="right" placeholder="库存"/>
               </template>
             </el-table-column>
             <el-table-column label="预设销量" prop="salePre" :show-overflow-tooltip="true" min-width="100">
               <template #default="scope">
-                <el-input size="small" type="number" v-model="scope.row.salePre" placeholder="预设销量"/>
+                <el-input-number size="small" type="number" v-model="scope.row.salePre" controls-position="right" placeholder="预设销量"/>
               </template>
             </el-table-column>
           </el-table>
@@ -138,7 +138,7 @@
 </template>
 
 <script setup name="ProductSku">
-import { skuStockList, productSave } from "@/api/product";
+import {skuStockList, productSave, productRemove} from "@/api/product";
 import ImageChoise from '@/views/components/ImageChoise';
 
 defineExpose({handleEdit})
@@ -152,12 +152,9 @@ const form = ref({
   skuStocks: []
 });
 
+const batchSkuData = ref({});
+
 const defaultProps = [
-  {id: 1, propName: '颜色', propItems: [{id: 1, itemValue: '白色'},{id: 2, itemValue: '黑色'}]},
-  {id: 2, propName: '尺寸', propItems: [{id: 3, itemValue: '中杯'},{id: 4, itemValue: '大杯'}, {id: 5, itemValue: '超大杯'}]},
-  {id: 3, propName: '糖份', propItems: [{id: 6, itemValue: '少糖'},{id: 7, itemValue: '多糖'}]},
-];
-const defaultPropsBack = [
   {propName: '颜色', propItems: [{itemValue: '白色'},{itemValue: '黑色'}]},
   {propName: '尺寸', propItems: [{itemValue: '中杯'},{itemValue: '大杯'}, {itemValue: '超大杯'}]},
   {propName: '糖份', propItems: [{itemValue: '少糖'},{itemValue: '多糖'}]},
@@ -214,23 +211,35 @@ function removeNode(node, data) {
 }
 
 function inputNode(node, data) {
-  console.log('inputNode', node, data);
   calcSkuStocks(form.value.props);
 }
 
-
 function calcSkuStocks(props) {
-  /*
-  const newSku = props[0].id === undefined;
-  const prop0Size = props.length === 1 ? props[0].propItems.length : 1;
-  const prop1Size = props.length === 2 ? props[1].propItems.length : 1;
-  const prop2Size = props.length === 3 ? props[2].propItems.length : 1;
-  const skuSize = prop0Size * prop1Size * prop2Size;
-  const skuStocks = newSku ? [skuSize]: form.value.skuStocks;
-   */
   const skuProps = cartesianProductOf(props);
-  console.log(rt);
+  const skuStocks = [];
+  for (let skuProp of skuProps) {
+    const sku = {};
+    let stockDesc = skuProp[0].itemValue;
 
+    sku.prop1Id = skuProp[0].propId;
+    sku.item1Id = skuProp[0].id;
+    sku.item1Desc = skuProp[0].itemValue;
+    if (skuProp.length > 1) {
+      stockDesc = stockDesc + '-' + skuProp[1].itemValue;
+      sku.prop2Id = skuProp[1].propId;
+      sku.item2Id = skuProp[1].id;
+      sku.item2Desc = skuProp[1].itemValue;
+    }
+    if (skuProp.length > 2) {
+      stockDesc = stockDesc + '-' + skuProp[2].itemValue;
+      sku.prop3Id = skuProp[2].propId;
+      sku.item3Id = skuProp[2].id;
+      sku.item3Desc = skuProp[2].itemValue;
+    }
+    sku.stockDesc = stockDesc;
+    skuStocks.push(sku);
+  }
+  form.value.skuStocks = skuStocks;
 }
 function cartesianProductOf(props) {
   return Array.prototype.reduce.call(props, function(a, b) {
@@ -242,6 +251,19 @@ function cartesianProductOf(props) {
     })
     return result
   }, [[]])
+}
+
+function batchSetSock() {
+  proxy.$modal.confirm('批量设置会覆盖所有已填写的内容，确定要批量设置吗？').then(() => {
+    const bsd = batchSkuData.value;
+    for (let sku of form.value.skuStocks) {
+      sku.originPrice = bsd.originPrice;
+      sku.promotionPrice = bsd.promotionPrice;
+      sku.deposit = bsd.deposit;
+      sku.stock = bsd.stock;
+      sku.salePre = bsd.salePre;
+    }
+  }).catch(() => {});
 }
 
 
